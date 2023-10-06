c63d05a2dac97ec06febfeb393e421ab97aceba5
Jefferson Alcantara <jeffersonalcantara26@gmail.com>
2023-10-05 21:14:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+
+  {
+    "id": "1",
+    "title": "Um novo modo de ver o mundo!",
+    "description": "As definições do universo foram redefinidas, a partir do momento que todos mudaram a sua visão sobre o que há fora do planeta terra...",
+    "photoCover": "https://s2.glbimg.com/CgM8-6p6wT8oNBzHC0qHbC01HLQ=/e.glbimg.com/og/ed/f/original/2020/02/03/andromeda-11004_1920.jpg"
+  },
+  {
+    "id": "2",
+    "title": "As mudanças no universo!",
+    "description": "As definições do universo foram redefinidas, a partir do momento que todos mudaram a sua visão sobre o que há fora do planeta terra...",
+    "photoCover": "https://www.hypeness.com.br/1/2022/07/30efc3ee-edit_james_webb_universo_hubble_ultra-deep.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Estamos mais próximos do mundo paralelo!",
+    "description": "O mundo paralelo e os seus desafios de um burraco negro e o espaço...",
+    "photoCover": "https://s2.glbimg.com/r1GkvDk7MA80azEaQrOQq_SvxSo=/smart/e.glbimg.com/og/ed/f/original/2020/05/18/gettyimages-151066380.jpg"
+  },
+  {
+    "id": "4",
+    "title": "O que acontece no mundo paralelo!",
+    "description": "A verdade por trás do universo paralelo que a Nasa teria descoberto...",
+    "photoCover": "https://s2.glbimg.com/syzd5ueFvVcpZj2zRx4GPgK1uHQ=/e.glbimg.com/og/ed/f/original/2019/03/22/15434397492_542f96604c_h.jpg"
+  }
+]
